8fcc7a70818ea789ced3dafe09b95d10801ae645
Dustin Carlino <dabreegster@gmail.com>
2024-11-08 12:52:16 +0000

    Sort OD stats

--- web/src/Stats.svelte
+++ web/src/Stats.svelte
@@ -25,7 +25,7 @@
   <details>
     <summary>Percent of demand by infrastructure type</summary>
     <ul>
-      {#each Object.entries($stats.od_percents_infra_type) as [key, percent]}
+      {#each Object.entries($stats.od_percents_infra_type).toSorted((a, b) => b[1] - a[1]) as [key, percent]}
         <li>{key}: {(100 * percent).toFixed(1)}%</li>
       {/each}
     </ul>
@@ -34,7 +34,7 @@
   <details>
     <summary>Percent of demand by level of service:</summary>
     <ul>
-      {#each Object.entries($stats.od_percents_los) as [key, percent]}
+      {#each Object.entries($stats.od_percents_los).toSorted((a, b) => b[1] - a[1]) as [key, percent]}
         <li>{key}: {(100 * percent).toFixed(1)}%</li>
       {/each}
     </ul>
