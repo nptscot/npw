970e65fadf875a1e4f114ab54433553446df10f5
Dustin Carlino <dabreegster@gmail.com>
2024-12-19 17:36:44 +0000

    Recalculate the LoS layer

--- web/src/layers/LevelOfService.svelte
+++ web/src/layers/LevelOfService.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import type { FeatureCollection } from "geojson";
   import { GeoJSON, hoverStateFilter, LineLayer } from "svelte-maplibre";
   import { backend, infraTypeMapping } from "../stores";
   import { Popup } from "svelte-utils/map";
@@ -6,11 +7,20 @@
   import { QualitativeLegend } from "../common";
   import { levelOfServiceColors, colorByLoS } from "../colors";
 
+  let data: FeatureCollection = {
+    type: "FeatureCollection",
+    features: [],
+  };
   let show = false;
-  let firstLoad = false;
 
-  $: if (show) {
-    firstLoad = true;
+  async function recalc() {
+    if ($backend) {
+      data = await $backend.renderLevelOfService();
+    }
+  }
+
+  $: if (show && data.features.length == 0) {
+    recalc();
   }
 </script>
 
@@ -21,45 +31,42 @@
   </label>
 
   {#if show}
+    <button class="outline" on:click={recalc}>Recalculate</button>
     <QualitativeLegend colors={levelOfServiceColors} />
   {/if}
 </LayerControls>
 
-{#if $backend && firstLoad}
-  {#await $backend.renderLevelOfService() then data}
-    <GeoJSON {data} generateId>
-      <LineLayer
-        layout={{
-          visibility: show ? "visible" : "none",
-        }}
-        paint={{
-          "line-width": hoverStateFilter(5, 7),
-          "line-color": colorByLoS,
-          "line-opacity": 0.8,
-        }}
-        manageHoverState
-      >
-        <Popup openOn="hover" let:props>
-          <table>
-            <tr>
-              <th>Level of Service</th>
-              <td>{props.los}</td>
-            </tr>
-            <tr>
-              <th>Infrastructure type</th>
-              <td>{infraTypeMapping[props.infra_type][0]}</td>
-            </tr>
-            <tr>
-              <th>Speed (mph)</th>
-              <td>{props.speed}</td>
-            </tr>
-            <tr>
-              <th>Estimated daily traffic volume</th>
-              <td>{props.traffic.toLocaleString()}</td>
-            </tr>
-          </table>
-        </Popup>
-      </LineLayer>
-    </GeoJSON>
-  {/await}
-{/if}
+<GeoJSON {data} generateId>
+  <LineLayer
+    layout={{
+      visibility: show ? "visible" : "none",
+    }}
+    paint={{
+      "line-width": hoverStateFilter(5, 7),
+      "line-color": colorByLoS,
+      "line-opacity": 0.8,
+    }}
+    manageHoverState
+  >
+    <Popup openOn="hover" let:props>
+      <table>
+        <tr>
+          <th>Level of Service</th>
+          <td>{props.los}</td>
+        </tr>
+        <tr>
+          <th>Infrastructure type</th>
+          <td>{infraTypeMapping[props.infra_type][0]}</td>
+        </tr>
+        <tr>
+          <th>Speed (mph)</th>
+          <td>{props.speed}</td>
+        </tr>
+        <tr>
+          <th>Estimated daily traffic volume</th>
+          <td>{props.traffic.toLocaleString()}</td>
+        </tr>
+      </table>
+    </Popup>
+  </LineLayer>
+</GeoJSON>
