181ca496e3db65b9119b7fced6171433e5cd77d2
Dustin Carlino <dabreegster@gmail.com>
2025-01-24 11:21:07 +0000

    Apply pico styles to a Modal

--- web/src/layers/ExistingNetwork.svelte
+++ web/src/layers/ExistingNetwork.svelte
@@ -55,34 +55,36 @@
 </LayerControls>
 
 {#if showImportModal}
-  <Modal on:close={() => (showImportModal = false)}>
-    <h2>Import existing routes from OpenStreetMap</h2>
+  <span class="pico">
+    <Modal on:close={() => (showImportModal = false)}>
+      <h2>Import existing routes from OpenStreetMap</h2>
 
-    <p>
-      This will add existing routes to your network. OpenStreetMap data and this
-      tool's interpretation of it are imperfect, so please check the results
-      carefully and remove / adjust any errors.
-    </p>
-    <p>
-      You can choose to import only Segregated Tracks (wide and narrow) and Off
-      Road Cycleways, or any infrastructure that achieves a high level of
-      service. For the latter option, if the result shows Painted Cycle Lanes,
-      then they are adequate to improve the level of service given the estimated
-      speed and traffic volumes.
-    </p>
+      <p>
+        This will add existing routes to your network. OpenStreetMap data and
+        this tool's interpretation of it are imperfect, so please check the
+        results carefully and remove / adjust any errors.
+      </p>
+      <p>
+        You can choose to import only Segregated Tracks (wide and narrow) and
+        Off Road Cycleways, or any infrastructure that achieves a high level of
+        service. For the latter option, if the result shows Painted Cycle Lanes,
+        then they are adequate to improve the level of service given the
+        estimated speed and traffic volumes.
+      </p>
 
-    <button on:click={() => importExisting("infra-type")}>
-      Import existing Segregated Tracks and Off Road Cycleways
-    </button>
-    <br />
-    <button on:click={() => importExisting("los")}>
-      Import existing routes that achieve a high level of service
-    </button>
-    <br />
-    <button class="secondary" on:click={() => (showImportModal = false)}>
-      Cancel
-    </button>
-  </Modal>
+      <button on:click={() => importExisting("infra-type")}>
+        Import existing Segregated Tracks and Off Road Cycleways
+      </button>
+      <br />
+      <button on:click={() => importExisting("los")}>
+        Import existing routes that achieve a high level of service
+      </button>
+      <br />
+      <button class="secondary" on:click={() => (showImportModal = false)}>
+        Cancel
+      </button>
+    </Modal>
+  </span>
 {/if}
 
 <!-- TODO For debugging -->
