cfbc4e4f95cc4569e3722c2d865a6dbe989936e7
Dustin Carlino <dabreegster@gmail.com>
2025-03-05 15:39:37 +0000

    Filter POIs by kind

--- web/src/layers/WarpToPOIs.svelte
+++ web/src/layers/WarpToPOIs.svelte
@@ -4,15 +4,26 @@
   import PrevNext from "../common/PrevNext.svelte";
   import { backend, currentStage, map, mutationCounter } from "../stores";
 
+  let filterKind: "school" | "gp" | "greenspace" | "all" = "all";
+
   let lastUpdate = 0;
+  let lastFilter = filterKind;
   let unreachablePOIs: [string, Position][] = [];
   let idx = 0;
 
   // TODO It'd be nice to sort these roughly by distance to the viewport?
-  async function recalc() {
-    if ($backend && lastUpdate != $mutationCounter) {
-      idx = 0;
-      unreachablePOIs = [];
+  async function recalc(filterKind: "school" | "gp" | "greenspace" | "all") {
+    if (
+      !$backend ||
+      (lastUpdate == $mutationCounter && lastFilter == filterKind)
+    ) {
+      return;
+    }
+
+    idx = 0;
+    unreachablePOIs = [];
+
+    if (filterKind == "all" || filterKind == "school") {
       for (let f of (await $backend.getSchools()).features) {
         if (!f.properties.reachable) {
           unreachablePOIs.push([
@@ -21,6 +32,9 @@
           ]);
         }
       }
+    }
+
+    if (filterKind == "all" || filterKind == "gp") {
       for (let f of (await $backend.getGpHospitals()).features) {
         if (!f.properties.reachable) {
           unreachablePOIs.push([
@@ -29,6 +43,9 @@
           ]);
         }
       }
+    }
+
+    if (filterKind == "all" || filterKind == "greenspace") {
       for (let f of (await $backend.getGreenspaces()).features) {
         if (f.properties.kind != "access point" && !f.properties.reachable) {
           // TODO Slow to calculate this constantly
@@ -38,14 +55,15 @@
           ]);
         }
       }
-
-      unreachablePOIs = unreachablePOIs;
-      lastUpdate = $mutationCounter;
     }
+
+    unreachablePOIs = unreachablePOIs;
+    lastUpdate = $mutationCounter;
+    lastFilter = filterKind;
   }
 
   $: if ($currentStage == "LocalAccess" && $mutationCounter > 0) {
-    recalc();
+    recalc(filterKind);
   }
 
   // TODO Never warp for 0, even if we go back to it
@@ -61,6 +79,16 @@
     });
   }
 </script>
+
+<label>
+  Filter POIs:
+  <select bind:value={filterKind}>
+    <option value="all">All</option>
+    <option value="school">Schools</option>
+    <option value="gp">GPs/hospitals</option>
+    <option value="greenspace">Greenspaces</option>
+  </select>
+</label>
 
 {#if unreachablePOIs.length > 0}
   <b>Fix next unreachable POI</b>
