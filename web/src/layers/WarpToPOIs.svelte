d59e850efe26b0ddb968fcfd43d231a782dd7170
Dustin Carlino <dabreegster@gmail.com>
2025-03-05 15:57:13 +0000

    Show both reachable and unreachable POIs

--- web/src/layers/WarpToPOIs.svelte
+++ web/src/layers/WarpToPOIs.svelte
@@ -4,30 +4,41 @@
   import PrevNext from "../common/PrevNext.svelte";
   import { backend, currentStage, map, mutationCounter } from "../stores";
 
-  let filterKind: "school" | "gp" | "greenspace" | "all" = "all";
+  type Reachability = "reachable" | "unreachable" | "all";
+  type Kind = "school" | "gp" | "greenspace" | "all";
+
+  let filterReachability: Reachability = "unreachable";
+  let filterKind: Kind = "all";
 
   let lastUpdate = 0;
-  let lastFilter = filterKind;
-  let unreachablePOIs: [string, Position][] = [];
+  let lastFilter: [Kind, Reachability] = [filterKind, filterReachability];
+  // [name, reachable, position]
+  let pois: [string, boolean, Position][] = [];
   let idx = 0;
 
   // TODO It'd be nice to sort these roughly by distance to the viewport?
-  async function recalc(filterKind: "school" | "gp" | "greenspace" | "all") {
+  async function recalc(filterKind: Kind, filterReachability: Reachability) {
     if (
       !$backend ||
-      (lastUpdate == $mutationCounter && lastFilter == filterKind)
+      (lastUpdate == $mutationCounter &&
+        lastFilter[0] == filterKind &&
+        lastFilter[1] == filterReachability)
     ) {
       return;
     }
 
     idx = 0;
-    unreachablePOIs = [];
+    pois = [];
 
     if (filterKind == "all" || filterKind == "school") {
       for (let f of (await $backend.getSchools()).features) {
-        if (!f.properties.reachable) {
-          unreachablePOIs.push([
+        if (
+          filterReachability == "all" ||
+          f.properties.reachable == (filterReachability == "reachable")
+        ) {
+          pois.push([
             f.properties.name || "This school",
+            f.properties.reachable,
             f.geometry.coordinates,
           ]);
         }
@@ -36,9 +47,13 @@
 
     if (filterKind == "all" || filterKind == "gp") {
       for (let f of (await $backend.getGpHospitals()).features) {
-        if (!f.properties.reachable) {
-          unreachablePOIs.push([
+        if (
+          filterReachability == "all" ||
+          f.properties.reachable == (filterReachability == "reachable")
+        ) {
+          pois.push([
             f.properties.name || "This GP or hospital",
+            f.properties.reachable,
             f.geometry.coordinates,
           ]);
         }
@@ -47,23 +62,30 @@
 
     if (filterKind == "all" || filterKind == "greenspace") {
       for (let f of (await $backend.getGreenspaces()).features) {
-        if (f.properties.kind != "access point" && !f.properties.reachable) {
+        if (f.properties.kind == "access point") {
+          continue;
+        }
+        if (
+          filterReachability == "all" ||
+          f.properties.reachable == (filterReachability == "reachable")
+        ) {
           // TODO Slow to calculate this constantly
-          unreachablePOIs.push([
+          pois.push([
             f.properties.name || "This greenspace",
+            f.properties.reachable!,
             centroid(f).geometry.coordinates,
           ]);
         }
       }
     }
 
-    unreachablePOIs = unreachablePOIs;
+    pois = pois;
     lastUpdate = $mutationCounter;
-    lastFilter = filterKind;
+    lastFilter = [filterKind, filterReachability];
   }
 
   $: if ($currentStage == "LocalAccess" && $mutationCounter > 0) {
-    recalc(filterKind);
+    recalc(filterKind, filterReachability);
   }
 
   // TODO Never warp for 0, even if we go back to it
@@ -74,11 +96,20 @@
       return;
     }
     $map.flyTo({
-      center: unreachablePOIs[idx][1] as [number, number],
+      center: pois[idx][2] as [number, number],
       zoom: 14,
     });
   }
 </script>
+
+<label>
+  Show reachable POIs:
+  <select bind:value={filterReachability}>
+    <option value="all">All</option>
+    <option value="reachable">Only reachable POIs</option>
+    <option value="unreachable">Only unreachable POIs</option>
+  </select>
+</label>
 
 <label>
   Filter POIs:
@@ -90,8 +121,19 @@
   </select>
 </label>
 
-{#if unreachablePOIs.length > 0}
-  <b>Fix next unreachable POI</b>
-  <p>{unreachablePOIs[idx][0]}</p>
-  <PrevNext list={unreachablePOIs} bind:idx />
+{#if pois.length > 0}
+  {#if pois[idx][1]}
+    <p>
+      {pois[idx][0]} is connected to the network. The blue path shows the route through
+      quiet streets to the network.
+    </p>
+  {:else}
+    <p>
+      {pois[idx][0]} is not connected to the network, because there are red severances
+      surronding it. Creating a local access route along the black path would fix
+      this.
+    </p>
+  {/if}
+
+  <PrevNext list={pois} bind:idx />
 {/if}
