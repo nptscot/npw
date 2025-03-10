3ef0e61335a56833b28467ab047a21f622e50886
Dustin Carlino <dabreegster@gmail.com>
2025-03-10 19:54:41 +0000

    Embed streetview. #67

--- web/src/layers/StreetViewPOI.svelte
+++ web/src/layers/StreetViewPOI.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
   import { Control } from "svelte-maplibre";
   import { currentPOI } from "./stores";
+
+  // Per https://developers.google.com/maps/documentation/embed/quickstart,
+  // make an API key and enable the Maps Embed API. This is apparently a
+  // free API.
+  let apiKey = "AIzaSyA5Na6sqIN4ctBGnA9nGJOiw8ImFcic2SI";
 </script>
 
 {#if $currentPOI}
   <Control position="bottom-left">
-    <div>
-      <a
-        href={`http://maps.google.com/maps?q=&layer=c&cbll=${$currentPOI.pt[1]},${$currentPOI.pt[0]}&cbp=11,0,0,0,0`}
-        target="_blank"
-      >
-        Streetview for {$currentPOI.pt}
-      </a>
-    </div>
+    <iframe
+      width="400"
+      height="200"
+      style:border="0"
+      loading="lazy"
+      allowfullscreen
+      title="Google Streetview"
+      src={`https://www.google.com/maps/embed/v1/streetview?key=${apiKey}&location=${$currentPOI.pt[1]},${$currentPOI.pt[0]}`}
+    />
   </Control>
 {/if}
-
-<style>
-  div {
-    background: white;
-  }
-</style>
