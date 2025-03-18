526f1871ab46e79c6fd3aad1e339e476188af694
Dustin Carlino <dabreegster@gmail.com>
2025-03-18 13:07:36 +0000

    Work on main mode styles

--- web/src/stats/LeftSidebarStats.svelte
+++ web/src/stats/LeftSidebarStats.svelte
@@ -36,87 +36,116 @@
 <Loading {loading} />
 
 {#if $stats}
-  <h4>Assess this tier</h4>
+  <div class="assess">
+    <h3>Assess this tier</h3>
 
-  {#if $currentStage == "Primary"}
-    <Metric
-      label="High cycling demand coverage"
-      pct={percent(
-        $stats.covered_demand_quintile_sums[0],
-        $stats.total_demand_quintile_sums[0],
-      )}
-    />
+    {#if $currentStage == "Primary"}
+      <Metric
+        label="High cycling demand coverage"
+        pct={percent(
+          $stats.covered_demand_quintile_sums[0],
+          $stats.total_demand_quintile_sums[0],
+        )}
+      />
 
-    <Metric
-      label="Main road coverage"
-      pct={percent(
-        $stats.covered_main_road_length,
-        $stats.total_main_road_length,
-      )}
-    />
-  {:else if $currentStage == "Secondary"}
-    <Metric
-      label="Medium cycling demand coverage"
-      pct={percent(
-        $stats.covered_demand_quintile_sums[1],
-        $stats.total_demand_quintile_sums[1],
-      )}
-    />
+      <Metric
+        label="Main road coverage"
+        pct={percent(
+          $stats.covered_main_road_length,
+          $stats.total_main_road_length,
+        )}
+      />
+    {:else if $currentStage == "Secondary"}
+      <Metric
+        label="Medium cycling demand coverage"
+        pct={percent(
+          $stats.covered_demand_quintile_sums[1],
+          $stats.total_demand_quintile_sums[1],
+        )}
+      />
 
-    <Metric label="Town centres" pct={$stats.percent_reachable_town_centres} />
-  {:else if $currentStage == "LocalAccess"}
-    <Metric label="Schools" pct={$stats.percent_reachable_schools} />
+      <Metric
+        label="Town centres"
+        pct={$stats.percent_reachable_town_centres}
+      />
+    {:else if $currentStage == "LocalAccess"}
+      <Metric label="Schools" pct={$stats.percent_reachable_schools} />
 
-    <Metric
-      label="GPs and hospitals"
-      pct={$stats.percent_reachable_gp_hospitals}
-    />
+      <Metric
+        label="GPs and hospitals"
+        pct={$stats.percent_reachable_gp_hospitals}
+      />
 
-    <Metric label="Greenspaces" pct={$stats.percent_reachable_greenspaces} />
+      <Metric label="Greenspaces" pct={$stats.percent_reachable_greenspaces} />
 
-    <Metric
-      label="Deprived population coverage"
-      pct={$stats.percent_reachable_imd_population}
-    />
+      <Metric
+        label="Deprived population coverage"
+        pct={$stats.percent_reachable_imd_population}
+      />
 
-    <Metric
-      label="Population coverage"
-      pct={$stats.percent_reachable_population}
-    />
-  {:else if $currentStage == "LongDistance"}
-    <Metric label="Settlements" pct={$stats.percent_reachable_settlements} />
-  {:else if $currentStage == "assessment"}
-    <FinalReport />
+      <Metric
+        label="Population coverage"
+        pct={$stats.percent_reachable_population}
+      />
+    {:else if $currentStage == "LongDistance"}
+      <Metric label="Settlements" pct={$stats.percent_reachable_settlements} />
+    {:else if $currentStage == "assessment"}
+      <FinalReport />
 
-    <button on:click={recalcOD} disabled={$mutationCounter == lastUpdateOD}>
-      Recalculate
-    </button>
+      <button on:click={recalcOD} disabled={$mutationCounter == lastUpdateOD}>
+        Recalculate
+      </button>
 
-    {#if $odStats}
-      <div style:margin-top="4px" style:border="2px solid black">
+      {#if $odStats}
+        <div style:margin-top="4px" style:border="2px solid black">
+          <p>
+            <!-- svelte-ignore a11y-invalid-attribute -->
+            <a
+              href="#"
+              on:click|preventDefault={() =>
+                ($mode = {
+                  kind: "evaluate-journey",
+                  prevMode: { kind: "main" },
+                  browse: notNull($odStats).worst_directness_routes,
+                })}
+            >
+              Average weighted directness
+            </a>
+            : {$odStats.average_weighted_directness.toFixed(1)}x
+          </p>
+
+          <ODBreakdowns od={$odStats} />
+        </div>
+      {:else}
         <p>
-          <!-- svelte-ignore a11y-invalid-attribute -->
-          <a
-            href="#"
-            on:click|preventDefault={() =>
-              ($mode = {
-                kind: "evaluate-journey",
-                prevMode: { kind: "main" },
-                browse: notNull($odStats).worst_directness_routes,
-              })}
-          >
-            Average weighted directness
-          </a>
-          : {$odStats.average_weighted_directness.toFixed(1)}x
+          Initial stats calculation disabled for faster start. Press the button
+          above.
         </p>
-
-        <ODBreakdowns od={$odStats} />
-      </div>
-    {:else}
-      <p>
-        Initial stats calculation disabled for faster start. Press the button
-        above.
-      </p>
+      {/if}
     {/if}
-  {/if}
+  </div>
 {/if}
+
+<style>
+  .assess {
+    margin-top: auto;
+    height: auto;
+    width: 100%;
+    padding: 0 20px;
+    border-top: 1px solid #ccc;
+    padding: 12px 20px 8px;
+  }
+
+  .assess :global(strong) {
+    float: right;
+  }
+
+  .assess :global(progress) {
+    width: 100%;
+    --color: var(--primary-tier-colour);
+  }
+  .assess :global(p) {
+    margin-bottom: 12px;
+    line-height: 20px;
+  }
+</style>
