13c9ed2a5828ebb754ccc01963cfdff237f5e698
Dustin Carlino <dabreegster@gmail.com>
2025-03-18 12:26:35 +0000

    Fix up top bar stats

--- web/src/stats/TopBarStats.svelte
+++ web/src/stats/TopBarStats.svelte
@@ -1,51 +1,89 @@
 <script lang="ts">
   import { stats } from "../stores";
-  import TerseMetric from "./TerseMetric.svelte";
 
   // Returns something [0, 1]
   function percent(x: number, total: number): number {
     if (total == 0) {
       return 0;
     }
-    return x / total;
+    return (100 * x) / total;
   }
 </script>
 
 {#if $stats}
-  <div class="progress" style:display="flex" style:gap="3em">
-    <TerseMetric
-      label="Safety"
-      pct={percent($stats.total_high_los_length, $stats.total_network_length)}
-      tooltip="What percent of your network has high Level of Service?"
-    />
+  <div class="progress-summary">
+    <ul>
+      <li title="What percent of your network has high Level of Service?">
+        Safety
+        <br />
+        <progress
+          value={percent(
+            $stats.total_high_los_length,
+            $stats.total_network_length,
+          )}
+          max="100"
+        />
+      </li>
 
-    <TerseMetric
-      label="Comfort"
-      pct={percent(
-        $stats.total_low_gradient_length,
-        $stats.total_network_length,
-      )}
-      tooltip="What percent of your network is on low gradient (&le; 3%)?"
-    />
+      <li title="What percent of your network is on low gradient (&le; 3%)?">
+        Comfort
+        <br />
+        <progress
+          value={percent(
+            $stats.total_low_gradient_length,
+            $stats.total_network_length,
+          )}
+          max="100"
+        />
+      </li>
 
-    <TerseMetric
-      label="Coherence (main roads)"
-      pct={percent(
-        $stats.covered_main_road_length,
-        $stats.total_main_road_length,
-      )}
-    />
+      <li>
+        Coherence (main roads)
+        <br />
+        <progress
+          value={percent(
+            $stats.covered_main_road_length,
+            $stats.total_main_road_length,
+          )}
+          max="100"
+        />
+      </li>
 
-    <div
-      style="display: flex; flex-direction: column"
-      title="Density of primary/secondary network within settlements"
-    >
-      <span>Coherence (density)</span>
-      {#if $stats.density_network_in_settlements}
-        <b>{Math.round($stats.density_network_in_settlements)}m</b>
-      {:else}
-        <b>no routes yet</b>
-      {/if}
-    </div>
+      <li title="Density of primary/secondary network within settlements">
+        Coherence (density)
+        <br />
+        {#if $stats.density_network_in_settlements}
+          {Math.round($stats.density_network_in_settlements)}m
+        {:else}
+          no routes yet
+        {/if}
+      </li>
+    </ul>
   </div>
 {/if}
+
+<style>
+  /* TODO Does this do anything? Why the big <a>?
+  .progress-summary a {
+    text-decoration: none;
+    color: #1a1a1a;
+    }*/
+  .progress-summary ul {
+    height: 100%;
+    list-style: none;
+    display: flex;
+    margin: 0;
+    padding: 0;
+  }
+  .progress-summary ul li {
+    list-style: none;
+    font-size: 1rem;
+    margin: 0;
+    width: 100px;
+    line-height: 12px;
+    padding: 12px 6px 4px;
+  }
+  .progress-summary ul li progress {
+    width: 100%;
+  }
+</style>
