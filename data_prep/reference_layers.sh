d8927d0376915212988490a08bd69e3f0fc77978
Dustin Carlino <dabreegster@gmail.com>
2025-03-24 11:52:10 +0000

    Update to March NPT data. No noticeable differences.

--- data_prep/reference_layers.sh
+++ data_prep/reference_layers.sh
@@ -6,27 +6,27 @@ mkdir -p tmp
 
 function core_net {
   # From https://github.com/nptscot/outputdata/releases
-  wget https://github.com/nptscot/outputdata/releases/download/v2025-02-01/combined_CN_4_2025-02-01_OS.gpkg -O tmp/core_network.gpkg
+  wget https://github.com/nptscot/outputdata/releases/download/v2025-03-01/combined_CN_4_2025-03-01_OS.gpkg -O tmp/core_network.gpkg
   # While we're still comparing map-matched results, include this too
-  wget https://github.com/nptscot/outputdata/releases/download/v2025-02-01/combined_CN_4_2025-02-01_OS.pmtiles -O ../web/public/core_network.pmtiles
+  wget https://github.com/nptscot/outputdata/releases/download/v2025-03-01/combined_CN_4_2025-03-01_OS.pmtiles -O ../web/public/core_network.pmtiles
 }
 
 function rnet {
   # From https://github.com/nptscot/outputdata/releases
 
   # For visualization
-  wget https://github.com/nptscot/outputdata/releases/download/v2025-02-01/rnet_2025-02-01.pmtiles -O ../web/public/route_network.pmtiles
+  wget https://github.com/nptscot/outputdata/releases/download/v2025-03-01/rnet_2025-03-01.pmtiles -O ../web/public/route_network.pmtiles
 
   # For analysis
-  wget https://github.com/nptscot/outputdata/releases/download/v2025-02-01/combined_network.gpkg -O tmp/combined_network.gpkg
+  wget https://github.com/nptscot/outputdata/releases/download/v2025-03-01/combined_network.gpkg -O tmp/combined_network.gpkg
 }
 
 function streetspace {
   # For visualization
-  wget https://github.com/nptscot/outputdata/releases/download/v2025-02-01/os_networks_categorized_street_space_with_widths.pmtiles -O ../web/public/streetspace.pmtiles
+  wget https://github.com/nptscot/outputdata/releases/download/v2025-03-01/os_networks_categorized_street_space_with_widths.pmtiles -O ../web/public/streetspace.pmtiles
 
   # For analysis
-  wget https://github.com/nptscot/outputdata/releases/download/v2025-02-01/os_networks_categorized_street_space_with_widths.gpkg -O tmp/streetspace.gpkg
+  wget https://github.com/nptscot/outputdata/releases/download/v2025-03-01/os_networks_categorized_street_space_with_widths.gpkg -O tmp/streetspace.gpkg
 }
 
 function schools {
