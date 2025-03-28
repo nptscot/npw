c9641baf5c3278b38eaf61fec944c49e7b0f5ea1
Dustin Carlino <dabreegster@gmail.com>
2025-03-28 16:04:43 +0000

    Start a printable report in a new tab

---
+++ web/src/assess/PrintableReport.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import ReportContents from "./ReportContents.svelte";
+
+  // TODO Having trouble getting SG css working on the new page; probably need to construct a <head> section properly
+</script>
+
+<header class="ds_page-header">
+  <h2 class="ds_page-header__title">Network Planning Workspace final report</h2>
+</header>
+
+<ReportContents />
