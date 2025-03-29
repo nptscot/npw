a3194d7989ffab8595c344d0fc43e079357e93f5
Dustin Carlino <dabreegster@gmail.com>
2025-03-29 10:36:14 +0000

    Pie charts for OD breakdowns

--- web/src/assess/ODBreakdowns.svelte
+++ web/src/assess/ODBreakdowns.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+  import {
+    ArcElement,
+    CategoryScale,
+    Chart as ChartJS,
+    Legend,
+    Title,
+    Tooltip,
+  } from "chart.js";
   import { onMount } from "svelte";
+  import { Pie } from "svelte-chartjs";
   import { notNull } from "svelte-utils";
   import { infraTypeColors, levelOfServiceColors, tierColors } from "../colors";
   import {
@@ -10,6 +19,8 @@
     odStats,
   } from "../stores";
   import { changePage } from "./index";
+
+  ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
 
   onMount(async () => {
     if ($lastUpdateOD != $mutationCounter) {
@@ -46,92 +57,58 @@
     : {$odStats.average_weighted_directness.toFixed(1)}x
   </p>
 
-  <div class="ds_accordion" data-module="ds-accordion">
-    <div class="ds_accordion-item">
-      <input
-        type="checkbox"
-        class="visually-hidden ds_accordion-item__control"
-        id="panel-1"
-        aria-labelledby="panel-1-heading"
-      />
-      <div class="ds_accordion-item__header">
-        <h2 id="panel-1-heading" class="ds_accordion-item__title">
-          Percent of demand by infrastructure type
-        </h2>
-        <span class="ds_accordion-item__indicator"></span>
-        <label class="ds_accordion-item__label" for="panel-1">
-          <span class="visually-hidden">Show this section</span>
-        </label>
-      </div>
-      <div class="ds_accordion-item__body">
-        <ul>
-          {#each Object.entries($odStats.od_percents_infra_type).toSorted((a, b) => b[1] - a[1]) as [key, pct]}
-            <li>
-              <span style:color={infraTypeColors[key] || "black"}>{key}</span>
-              : {Math.round(pct * 100)}%
-            </li>
-          {/each}
-        </ul>
-      </div>
-    </div>
+  <h4>Percent of demand by level of service</h4>
+  <div style:height="300px">
+    <Pie
+      data={{
+        labels: Object.keys($odStats.od_percents_los),
+        datasets: [
+          {
+            data: Object.values($odStats.od_percents_los),
+            backgroundColor: Object.keys($odStats.od_percents_los).map(
+              (key) => levelOfServiceColors[key],
+            ),
+          },
+        ],
+      }}
+      options={{ responsive: true }}
+    />
+  </div>
 
-    <div class="ds_accordion-item">
-      <input
-        type="checkbox"
-        class="visually-hidden ds_accordion-item__control"
-        id="panel-2"
-        aria-labelledby="panel-2-heading"
-      />
-      <div class="ds_accordion-item__header">
-        <h2 id="panel-2-heading" class="ds_accordion-item__title">
-          Percent of demand by tier
-        </h2>
-        <span class="ds_accordion-item__indicator"></span>
-        <label class="ds_accordion-item__label" for="panel-2">
-          <span class="visually-hidden">Show this section</span>
-        </label>
-      </div>
-      <div class="ds_accordion-item__body">
-        <ul>
-          {#each Object.entries($odStats.od_percents_tier).toSorted((a, b) => b[1] - a[1]) as [key, pct]}
-            <li>
-              <span style:color={tierColors[key] || "black"}>{key}</span>
-              : {Math.round(pct * 100)}%
-            </li>
-          {/each}
-        </ul>
-      </div>
-    </div>
+  <h4>Percent of demand by infrastructure type</h4>
+  <div style:height="300px">
+    <Pie
+      data={{
+        labels: Object.keys($odStats.od_percents_infra_type),
+        datasets: [
+          {
+            data: Object.values($odStats.od_percents_infra_type),
+            backgroundColor: Object.keys($odStats.od_percents_infra_type).map(
+              (key) => infraTypeColors[key] || "black",
+            ),
+          },
+        ],
+      }}
+      options={{ responsive: true }}
+    />
+  </div>
 
-    <div class="ds_accordion-item">
-      <input
-        type="checkbox"
-        class="visually-hidden ds_accordion-item__control"
-        id="panel-3"
-        aria-labelledby="panel-3-heading"
-      />
-      <div class="ds_accordion-item__header">
-        <h2 id="panel-3-heading" class="ds_accordion-item__title">
-          Percent of demand by level of service
-        </h2>
-        <span class="ds_accordion-item__indicator"></span>
-        <label class="ds_accordion-item__label" for="panel-3">
-          <span class="visually-hidden">Show this section</span>
-        </label>
-      </div>
-      <div class="ds_accordion-item__body">
-        <ul>
-          {#each Object.entries($odStats.od_percents_los).toSorted((a, b) => b[1] - a[1]) as [key, pct]}
-            <li>
-              <span style:color={levelOfServiceColors[key] || "black"}>
-                {key}
-              </span>
-              : {Math.round(pct * 100)}%
-            </li>
-          {/each}
-        </ul>
-      </div>
-    </div>
+  <h4>Percent of demand by tier</h4>
+  <div style:height="300px">
+    <Pie
+      data={{
+        labels: Object.keys($odStats.od_percents_tier),
+        datasets: [
+          {
+            data: Object.values($odStats.od_percents_tier),
+            backgroundColor: Object.keys($odStats.od_percents_tier).map(
+              (key) => tierColors[key] || "black",
+            ),
+          },
+        ],
+      }}
+      options={{ responsive: true }}
+    />
   </div>
 {:else}
   <p>Recalculating...</p>
