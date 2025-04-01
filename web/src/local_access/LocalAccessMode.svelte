91ec03fd2371bc55e3a358cbf12d9f12f1307307
Dustin Carlino <dabreegster@gmail.com>
2025-04-01 11:46:24 +0100

    Move the filtering/warping code into the main mode, reorg controls slightly

--- web/src/local_access/LocalAccessMode.svelte
+++ web/src/local_access/LocalAccessMode.svelte
@@ -1,14 +1,115 @@
 <script lang="ts">
   import { MapEvents } from "svelte-maplibre";
+  import { tierColors } from "../colors";
   import { HelpButton } from "../common";
   import { SplitComponent } from "../common/layout";
   import LeftSidebarStats from "../stats/LeftSidebarStats.svelte";
-  import { autosave, backend, devMode, mode } from "../stores";
+  import {
+    autosave,
+    backend,
+    currentStage,
+    devMode,
+    map,
+    mode,
+    mutationCounter,
+  } from "../stores";
+  import type { PoiKind } from "../types";
   import Greenspaces from "./Greenspaces.svelte";
   import PointPOIs from "./PointPOIs.svelte";
-  import { currentPOI } from "./stores";
+  import { currentPOI, type POI } from "./stores";
   import StreetViewPOI from "./StreetViewPOI.svelte";
-  import WarpToPOIs from "./WarpToPOIs.svelte";
+
+  let lastUpdate = 0;
+
+  let allPOIs: POI[] = [];
+
+  let filterKind: PoiKind | "all" = "all";
+  let filterIdx = 0;
+  let filteredPOIs: POI[] = [];
+
+  async function recalc() {
+    if (!$backend || lastUpdate == $mutationCounter) {
+      return;
+    }
+
+    let list: [POI, number, number][] = [];
+
+    for (let f of (await $backend.getPOIs()).features) {
+      list.push([
+        {
+          kind: f.properties.poi_kind,
+          idx: f.properties.idx,
+          description: f.properties.description,
+          reachable: f.properties.reachable,
+          pt: f.geometry.coordinates as [number, number],
+        },
+        f.properties.poi_kind == "schools" ? 1 : 2,
+        f.properties.sort,
+      ]);
+    }
+
+    for (let f of (await $backend.getGreenspaces()).features) {
+      if (f.properties.kind == "access point") {
+        continue;
+      }
+      list.push([
+        {
+          kind: f.properties.poi_kind,
+          idx: f.properties.idx!,
+          description: f.properties.description!,
+          reachable: f.properties.reachable!,
+          pt: f.properties.centroid!,
+        },
+        3,
+        f.properties.sort!,
+      ]);
+    }
+
+    // Prioritize schools, then GPs, then greenspaces. Within each group, use a
+    // Hilbert curve to group nearby POIs together.
+    list.sort((a, b) => {
+      if (a[1] != b[1]) {
+        return a[1] - b[1];
+      }
+      return a[2] - b[2];
+    });
+
+    allPOIs = list.map(([poi, _a, _b]) => poi);
+    lastUpdate = $mutationCounter;
+
+    refilterPOIs();
+  }
+  $: if ($currentStage == "LocalAccess" && $mutationCounter > 0) {
+    recalc();
+  }
+
+  function refilterPOIs() {
+    filterIdx = 0;
+    let unconnected = allPOIs.filter((poi) => !poi.reachable);
+    filteredPOIs =
+      filterKind == "all"
+        ? unconnected
+        : unconnected.filter((poi) => poi.kind == filterKind);
+  }
+
+  function warp() {
+    if ($map && $currentPOI) {
+      $map.easeTo({
+        center: $currentPOI.pt,
+      });
+    }
+  }
+
+  function findAnother() {
+    if (filterIdx != filteredPOIs.length - 1) {
+      filterIdx++;
+    } else {
+      filterIdx = 0;
+    }
+
+    $currentPOI = filteredPOIs[filterIdx];
+    warp();
+  }
 
   async function fixUnreachable() {
     if ($currentPOI) {
@@ -30,6 +131,14 @@
       if (tag != "INPUT") {
         e.preventDefault();
         await fixUnreachable();
+      }
+    }
+
+    if (e.key == "n" && filteredPOIs.length > 0) {
+      let tag = (e.target as HTMLElement).tagName;
+      if (tag != "INPUT") {
+        e.preventDefault();
+        findAnother();
       }
     }
   }
@@ -71,21 +180,60 @@
           shown in red and need to be fixed.
         </p>
 
-        <p>TODO filter</p>
+        <label class="ds_label" for="filterKind">Filter POIs</label>
+        <div class="ds_select-wrapper ds_input--fluid-one-third">
+          <select
+            class="ds_select"
+            id="filterKind"
+            name="filterKind"
+            bind:value={filterKind}
+            on:change={refilterPOIs}
+          >
+            <option value="all">All</option>
+            <option value="schools">Schools</option>
+            <option value="gp_hospitals">GPs/hospitals</option>
+            <option value="greenspaces">Greenspaces</option>
+          </select>
+          <span class="ds_select-arrow" aria-hidden="true"></span>
+        </div>
 
-        <p>TODO main button</p>
+        {#if filteredPOIs.length > 0}
+          <div>
+            <button class="ds_button" on:click={findAnother}>
+              Fix connectivity for remaining POIs
+            </button>
+          </div>
+        {:else}
+          <p>All POIs are connected.</p>
+        {/if}
       {:else}
         <header class="ds_page-header">
-          <span class="ds_page-header__label ds_content-label">
+          <span
+            class="ds_page-header__label ds_content-label"
+            style:color={tierColors.LocalAccess}
+          >
             Local Access
           </span>
           <h2 class="ds_page-header__title">Fix connectivity for a POI</h2>
         </header>
 
+        <div>
+          <button
+            type="button"
+            class="ds_link"
+            on:click={() => ($currentPOI = null)}
+          >
+            <i class="fa-solid fa-chevron-left"></i>
+            Filter for different POIs
+          </button>
+        </div>
+
         {#if $currentPOI.reachable}
           <p>
-            {$currentPOI.description} is connected to the network. The blue path
-            shows the route through quiet streets to the network.
+            {$currentPOI.description} is connected to the network.
+          </p>
+          <p>
+            The blue path shows the route through quiet streets to the network.
           </p>
         {:else}
           <p>
@@ -109,9 +257,16 @@
             </button>
           </div>
         {/if}
-      {/if}
 
-      <WarpToPOIs />
+        <button
+          class="ds_button ds_button--secondary"
+          disabled={filteredPOIs.length == 0}
+          on:click={findAnother}
+          style:float="right"
+        >
+          Next POI (n)
+        </button>
+      {/if}
     </div>
 
     <LeftSidebarStats />
