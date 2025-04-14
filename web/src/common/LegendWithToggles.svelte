70b9b8520e1de2eca23f03ccd22d85a2a6f306d3
Dustin Carlino <dabreegster@gmail.com>
2025-04-14 10:57:52 +0100

    Make the bottom panel text color be white or black, depending on the background color. It maybe looks odd to mix both, but at least all the text is legible now.

--- web/src/common/LegendWithToggles.svelte
+++ web/src/common/LegendWithToggles.svelte
@@ -4,11 +4,20 @@
   export let labels: { [name: string]: string };
   export let colors: { [name: string]: string };
   export let show: Writable<{ [name: string]: boolean }>;
+
+  // https://stackoverflow.com/questions/27869740/find-the-correct-wcag-aa-contrast-color-for-a-given-hex-background-value
+  function contrastColor(hex: string): string {
+    let r = parseInt(hex.substr(1, 2), 16);
+    let g = parseInt(hex.substr(3, 2), 16);
+    let b = parseInt(hex.substr(5, 2), 16);
+    let brightness = (r * 299 + g * 587 + b * 114) / 1000;
+    return brightness > 128 ? "#000000" : "#ffffff";
+  }
 </script>
 
 <ul>
   {#each Object.entries(colors) as [key, color]}
-    <li style:background-color={color}>
+    <li style:background-color={color} style:color={contrastColor(color)}>
       <label>
         <input type="checkbox" bind:checked={$show[key]} />
         {labels[key]}
