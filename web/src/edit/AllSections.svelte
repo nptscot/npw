8fea1c58a9d8c56c2b600c087efec8e8c1f0c539
Dustin Carlino <dabreegster@gmail.com>
2025-04-15 11:40:38 +0100

    Split out a component for the drawing section breakdowns

---
+++ web/src/edit/AllSections.svelte
@@ -0,0 +1,197 @@
+<script lang="ts">
+  import { tierLabels } from "../colors";
+  import { Modal, percent } from "../common";
+  import { editModeBreakdown } from "../stores";
+  import type { AutosplitRoute, Tier } from "../types";
+  import PickInfraType from "./PickInfraType.svelte";
+  import SectionDiagram from "./SectionDiagram.svelte";
+
+  export let sectionsGj: AutosplitRoute;
+  export let infraType: string;
+  export let overrideInfraType: boolean;
+  export let tier: Tier;
+
+  let showOverrideModal = false;
+
+  $: pctFits = percentFits(sectionsGj);
+  $: pctHighLoS = percentHighLoS(sectionsGj);
+  $: pctMatchesTier = percentMatchesTier(sectionsGj);
+
+  function percentFits(sectionsGj: AutosplitRoute): string {
+    let total = 0;
+    let fits = 0;
+    for (let f of sectionsGj.features) {
+      total += f.properties.length;
+      if (f.properties.fits) {
+        fits += f.properties.length;
+      }
+    }
+    return percent(fits, total);
+  }
+
+  function percentHighLoS(sectionsGj: AutosplitRoute): string {
+    let total = 0;
+    let high = 0;
+    for (let f of sectionsGj.features) {
+      total += f.properties.length;
+      if (f.properties.los == "High") {
+        high += f.properties.length;
+      }
+    }
+    return percent(high, total);
+  }
+
+  function percentMatchesTier(sectionsGj: AutosplitRoute): string {
+    let total = 0;
+    let matches = 0;
+    for (let f of sectionsGj.features) {
+      total += f.properties.length;
+      if (f.properties.tier == tier) {
+        matches += f.properties.length;
+      }
+    }
+    return percent(matches, total);
+  }
+</script>
+
+<section>
+  <h4>
+    <!-- svelte-ignore a11y-invalid-attribute -->
+    <a
+      href="#"
+      on:click|preventDefault={() => ($editModeBreakdown = "infra_type")}
+      class:focused={$editModeBreakdown == "infra_type"}
+    >
+      Infrastructure type
+    </a>
+  </h4>
+
+  <SectionDiagram breakdown="infra_type" {sectionsGj} />
+
+  {#if overrideInfraType}
+    <p>
+      You've forced this route to always use {infraType}, assuming high Level of
+      Service.
+    </p>
+
+    <button
+      class="ds_button ds_button--secondary"
+      on:click={() => (overrideInfraType = false)}
+    >
+      Remove override
+    </button>
+  {:else}
+    <p>
+      The route you've drawn has been split into sections, automatically picking
+      an infrastructure type to achieve the best possible Level of Service.
+    </p>
+
+    <button
+      class="ds_button ds_button--secondary"
+      on:click={() => {
+        overrideInfraType = true;
+        showOverrideModal = true;
+      }}
+    >
+      Override infrastructure type
+    </button>
+  {/if}
+</section>
+
+<section>
+  <!-- svelte-ignore a11y-invalid-attribute -->
+  <h4>
+    <a
+      href="#"
+      on:click|preventDefault={() => ($editModeBreakdown = "deliverability")}
+      class:focused={$editModeBreakdown == "deliverability"}
+    >
+      Deliverability
+    </a>
+  </h4>
+
+  <SectionDiagram breakdown="deliverability" {sectionsGj} />
+
+  {#if pctFits != "100%"}
+    <p>
+      Only {pctFits} of the route fits in the available streetspace. You may need
+      to override the infrastructure type for some sections.
+    </p>
+  {/if}
+</section>
+
+<section>
+  <h4>
+    <!-- svelte-ignore a11y-invalid-attribute -->
+    <a
+      href="#"
+      on:click|preventDefault={() => ($editModeBreakdown = "los")}
+      class:focused={$editModeBreakdown == "los"}
+    >
+      Level of Service
+    </a>
+  </h4>
+
+  <SectionDiagram breakdown="los" {sectionsGj} />
+
+  {#if pctHighLoS != "100%"}
+    <p>
+      Only {pctHighLoS} of the route has a high level of service. You may need to
+      override the infrastructure type for some sections and reduce traffic speeds
+      and volumes.
+    </p>
+  {/if}
+</section>
+
+<section>
+  <h4>
+    <!-- svelte-ignore a11y-invalid-attribute -->
+    <a
+      href="#"
+      on:click|preventDefault={() => ($editModeBreakdown = "tier")}
+      class:focused={$editModeBreakdown == "tier"}
+    >
+      Tier
+    </a>
+  </h4>
+
+  <SectionDiagram breakdown="tier" {sectionsGj} />
+
+  {#if pctMatchesTier != "100%"}
+    <p>
+      Only {pctMatchesTier} of the route will use the {tierLabels[tier]} tier. Because
+      the route enters and exits settlements, part of it is assigned to a different
+      tier.
+    </p>
+  {/if}
+</section>
+
+<section>
+  <h4>
+    <!-- svelte-ignore a11y-invalid-attribute -->
+    <a
+      href="#"
+      on:click|preventDefault={() => ($editModeBreakdown = "gradient")}
+      class:focused={$editModeBreakdown == "gradient"}
+    >
+      Gradient
+    </a>
+  </h4>
+
+  <SectionDiagram breakdown="gradient" {sectionsGj} />
+</section>
+
+<Modal bind:show={showOverrideModal}>
+  <PickInfraType
+    onFinish={(value) => {
+      infraType = value;
+      showOverrideModal = false;
+    }}
+  />
+</Modal>
+
+<style>
+  .focused {
+    text-decoration: underline;
+  }
+</style>
--- web/src/edit/EditRouteMode.svelte
+++ web/src/edit/EditRouteMode.svelte
@@ -8,9 +8,8 @@
     infraTypeColors,
     levelOfServiceColors,
     tierColors,
-    tierLabels,
   } from "../colors";
-  import { layerId, Modal, percent } from "../common";
+  import { layerId } from "../common";
   import RelevantLayers from "../layers/RelevantLayers.svelte";
   import LeftSidebarStats from "../stats/LeftSidebarStats.svelte";
   import {
@@ -21,9 +20,8 @@
     mode,
   } from "../stores";
   import type { AutosplitRoute, InfraType, Waypoint } from "../types";
-  import PickInfraType from "./PickInfraType.svelte";
+  import AllSections from "./AllSections.svelte";
   import RouteControls from "./RouteControls.svelte";
-  import SectionDiagram from "./SectionDiagram.svelte";
   import { waypoints } from "./stores";
 
   export let map: Map;
@@ -37,8 +35,6 @@
   let infraType = "MixedTraffic";
   let overrideInfraType = false;
   let tier = $currentStage == "assessment" ? "Primary" : $currentStage;
-
-  let showOverrideModal = false;
 
   let sectionsGj: AutosplitRoute = emptyGeojson() as AutosplitRoute;
   $: recalculateSections($waypoints, overrideInfraType, infraType);
@@ -99,42 +95,6 @@
     } catch (err) {}
   }
 
-  function percentFits(sectionsGj: AutosplitRoute): string {
-    let total = 0;
-    let fits = 0;
-    for (let f of sectionsGj.features) {
-      total += f.properties.length;
-      if (f.properties.fits) {
-        fits += f.properties.length;
-      }
-    }
-    return percent(fits, total);
-  }
-
-  function percentHighLoS(sectionsGj: AutosplitRoute): string {
-    let total = 0;
-    let high = 0;
-    for (let f of sectionsGj.features) {
-      total += f.properties.length;
-      if (f.properties.los == "High") {
-        high += f.properties.length;
-      }
-    }
-    return percent(high, total);
-  }
-
-  function percentMatchesTier(sectionsGj: AutosplitRoute): string {
-    let total = 0;
-    let matches = 0;
-    for (let f of sectionsGj.features) {
-      total += f.properties.length;
-      if (f.properties.tier == tier) {
-        matches += f.properties.length;
-      }
-    }
-    return percent(matches, total);
-  }
-
   let colorSections = {
     infra_type: constructMatchExpression(
       ["get", "infra_type"],
@@ -172,138 +132,7 @@
 >
   <div slot="extra-controls">
     {#if $waypoints.length >= 2}
-      {@const pctFits = percentFits(sectionsGj)}
-      {@const pctHighLoS = percentHighLoS(sectionsGj)}
-      {@const pctMatchesTier = percentMatchesTier(sectionsGj)}
-
-      <section>
-        <h4>
-          <!-- svelte-ignore a11y-invalid-attribute -->
-          <a
-            href="#"
-            on:click|preventDefault={() => ($editModeBreakdown = "infra_type")}
-            class:focused={$editModeBreakdown == "infra_type"}
-          >
-            Infrastructure type
-          </a>
-        </h4>
-
-        <SectionDiagram breakdown="infra_type" {sectionsGj} />
-
-        {#if overrideInfraType}
-          <p>
-            You've forced this route to always use {infraType}, assuming high
-            Level of Service.
-          </p>
-
-          <button
-            class="ds_button ds_button--secondary"
-            on:click={() => (overrideInfraType = false)}
-          >
-            Remove override
-          </button>
-        {:else}
-          <p>
-            The route you've drawn has been split into sections, automatically
-            picking an infrastructure type to achieve the best possible Level of
-            Service.
-          </p>
-
-          <button
-            class="ds_button ds_button--secondary"
-            on:click={() => {
-              overrideInfraType = true;
-              showOverrideModal = true;
-            }}
-          >
-            Override infrastructure type
-          </button>
-        {/if}
-      </section>
-
-      <section>
-        <!-- svelte-ignore a11y-invalid-attribute -->
-        <h4>
-          <a
-            href="#"
-            on:click|preventDefault={() =>
-              ($editModeBreakdown = "deliverability")}
-            class:focused={$editModeBreakdown == "deliverability"}
-          >
-            Deliverability
-          </a>
-        </h4>
-
-        <SectionDiagram breakdown="deliverability" {sectionsGj} />
-
-        {#if pctFits != "100%"}
-          <p>
-            Only {pctFits} of the route fits in the available streetspace. You may
-            need to override the infrastructure type for some sections.
-          </p>
-        {/if}
-      </section>
-
-      <section>
-        <h4>
-          <!-- svelte-ignore a11y-invalid-attribute -->
-          <a
-            href="#"
-            on:click|preventDefault={() => ($editModeBreakdown = "los")}
-            class:focused={$editModeBreakdown == "los"}
-          >
-            Level of Service
-          </a>
-        </h4>
-
-        <SectionDiagram breakdown="los" {sectionsGj} />
-
-        {#if pctHighLoS != "100%"}
-          <p>
-            Only {pctHighLoS} of the route has a high level of service. You may need
-            to override the infrastructure type for some sections and reduce traffic
-            speeds and volumes.
-          </p>
-        {/if}
-      </section>
-
-      <section>
-        <h4>
-          <!-- svelte-ignore a11y-invalid-attribute -->
-          <a
-            href="#"
-            on:click|preventDefault={() => ($editModeBreakdown = "tier")}
-            class:focused={$editModeBreakdown == "tier"}
-          >
-            Tier
-          </a>
-        </h4>
-
-        <SectionDiagram breakdown="tier" {sectionsGj} />
-
-        {#if pctMatchesTier != "100%"}
-          <p>
-            Only {pctMatchesTier} of the route will use the {tierLabels[tier]} tier.
-            Because the route enters and exits settlements, part of it is assigned
-            to a different tier.
-          </p>
-        {/if}
-      </section>
-
-      <section>
-        <h4>
-          <!-- svelte-ignore a11y-invalid-attribute -->
-          <a
-            href="#"
-            on:click|preventDefault={() => ($editModeBreakdown = "gradient")}
-            class:focused={$editModeBreakdown == "gradient"}
-          >
-            Gradient
-          </a>
-        </h4>
-
-        <SectionDiagram breakdown="gradient" {sectionsGj} />
-      </section>
+      <AllSections {sectionsGj} bind:infraType bind:overrideInfraType {tier} />
 
       <h4>Route properties</h4>
 
@@ -334,15 +163,6 @@
     {/if}
 
     <RelevantLayers />
-
-    <Modal bind:show={showOverrideModal}>
-      <PickInfraType
-        onFinish={(value) => {
-          infraType = value;
-          showOverrideModal = false;
-        }}
-      />
-    </Modal>
   </div>
 
   <svelte:fragment slot="sticky-bottom-controls">
@@ -361,9 +181,3 @@
     </GeoJSON>
   </span>
 </RouteControls>
-
-<style>
-  .focused {
-    text-decoration: underline;
-  }
-</style>
