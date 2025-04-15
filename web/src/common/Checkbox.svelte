d064435d281dad35cecb243c9242578a8bdcfe09
Dustin Carlino <dabreegster@gmail.com>
2025-04-15 11:06:55 +0100

    Restore govuk checkboxes, now that we're not playing weird tricks with the DOM

--- web/src/common/Checkbox.svelte
+++ web/src/common/Checkbox.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
+  import { v4 as uuidv4 } from "uuid";
+
   export let checked: boolean;
   export let small = false;
   // The slot is the label
 
-  if (small && false) {
-  }
+  let id = uuidv4();
 </script>
 
-<label>
-  <input bind:checked type="checkbox" />
-  <slot />
-</label>
+<div class:ds_checkbox={true} class:ds_checkbox--small={small}>
+  <input
+    bind:checked
+    class="ds_checkbox__input"
+    {id}
+    name={id}
+    type="checkbox"
+  />
+  <label class="ds_checkbox__label" for={id}>
+    <slot />
+  </label>
+</div>
