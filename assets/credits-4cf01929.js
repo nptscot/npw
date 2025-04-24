7d7a9df9750544c90a6243c235a988ccbdd932ea
dabreegster <dabreegster@users.noreply.github.com>
2025-04-24 09:40:19 +0000

    deploy: d314e70d8afe8cf73a311db48215caee404a6665

---
+++ assets/credits-4cf01929.js
@@ -0,0 +1,15 @@
+import{S as y,i as v,s as S,e as l,a as o,g as a,w as k,o as n,u as C}from"./index-36380ea8.js";import"./design-system-57630050.js";/* empty css                */function T(_){let i,w,s,h,r,f,p,g,u,b,c,m,d;return{c(){i=l("h1"),i.textContent="Network Planning Workspace",w=o(),s=l("div"),s.innerHTML=`<a href="./"><i class="fa-solid fa-chevron-left"></i>
+    Back to NPW</a>`,h=o(),r=l("p"),r.textContent=`The NPW is designed to enable local authorities to plan a cycle network for
+  the area, using segregated infrastructure on key routes and ensuring local
+  places are properly connected without severance.`,f=o(),p=l("p"),p.innerHTML=`The NPW is an <a href="https://nptscot.github.io/npw/" target="_blank">open source project</a>
+  funded by
+  <a href="https://www.transport.gov.scot/" target="_blank">Transport Scotland</a>
+  and designed by
+  <a href="https://www.sustrans.org.uk" target="_blank">Sustrans Scotland</a>
+  . The team includes:`,g=o(),u=l("ul"),u.innerHTML=`<li>Dustin Carlino, lead developer and director of <a href="https://find-and-update.company-information.service.gov.uk/company/16217327" target="_blank">A/B Street Ltd</a></li> <li><a href="https://www.cyclestreets.org/" target="_blank">CycleStreets Ltd</a>
+    , UX/workflow design</li> <li>Sustrans Scotland
+    <ul><li><a href="https://www.linkedin.com/in/congying-hu/" target="_blank">Congying (Cici) Hu</a>
+        , mobility planner</li> <li><a href="https://www.linkedin.com/in/angus-calder-a69543206" target="_blank">Angus Calder</a>
+        , senior mobility planner</li> <li><a href="https://www.linkedin.com/in/matthew-davis-6b3a8124/" target="_blank">Matthew Davis</a>
+        , principal mobility planner</li></ul></li> <li>ITS Leeds data science
+    <ul><li><a href="https://www.robinlovelace.net/" target="_blank">Robin Lovelace</a></li> <li><a href="https://github.com/wangzhao0217" target="_blank">Zhao Wang</a></li> <li><a href="https://github.com/juanfonsecaLS1" target="_blank">Juan P. Fonseca-Zamora</a></li></ul></li>`,b=o(),c=l("p"),c.textContent="This tool would not be possible without:",m=o(),d=l("ul"),d.innerHTML='<li><a href="https://www.openstreetmap.org/about" target="_blank">OpenStreetMap contributors</a></li> <li><a href="https://www.maptiler.com/" target="_blank">Maptiler</a></li> <li><a href="https://georust.org/" target="_blank">GeoRust</a></li> <li><a href="https://fontawesome.com/" target="_blank">Font Awesome icons</a></li> <li>And countless other open source libraries</li>'},m(e,t){a(e,i,t),a(e,w,t),a(e,s,t),a(e,h,t),a(e,r,t),a(e,f,t),a(e,p,t),a(e,g,t),a(e,u,t),a(e,b,t),a(e,c,t),a(e,m,t),a(e,d,t)},p:k,i:k,o:k,d(e){e&&(n(i),n(w),n(s),n(h),n(r),n(f),n(p),n(g),n(u),n(b),n(c),n(m),n(d))}}}function L(_){return C(async()=>{window.DS.initAll()}),[]}class M extends y{constructor(i){super(),v(this,i,L,T,S,{})}}new M({target:document.getElementById("app")});
