356a0c9f8682017122cfb2876eda124ab0663f08
Dustin Carlino <dabreegster@gmail.com>
2025-04-28 13:19:36 +0100

    Try again for GH actions -- ignore old branches without the new manual script

--- release/gh_build.sh
+++ release/gh_build.sh
@@ -20,8 +20,8 @@ for branch in main demo nptless_demo; do
   # new URL('backend_bg.wasm', import.meta.url) doesn't exist at build time, it will remain unchanged to be resolved at runtime
   ln -s assets/backend_bg.wasm dist/
 
-  # Writes to dist/
-  npm run generate-manual
+  # Writes to dist/. If the script is missing in some old branches, don't fail.
+  npm run generate-manual || echo
 
   if [ "$branch" == "main" ]; then
     mv dist/* deployme
