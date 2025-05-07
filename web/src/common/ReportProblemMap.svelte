e20122ade40b1212ab6a644411b97ca2942999b5
Dustin Carlino <dabreegster@gmail.com>
2025-05-07 09:53:22 +0100

    Don't let people report problems at all at low zoom

--- web/src/common/ReportProblemMap.svelte
+++ web/src/common/ReportProblemMap.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
-  import { showReportProblem } from "../stores";
+  import { showReportProblem, zoom } from "../stores";
 
   // There are two components for this, so the modal can exist outside the map
   // and use SG styles.
 </script>
 
 <div>
-  <button type="button" on:click={() => ($showReportProblem = true)}>
+  <button
+    type="button"
+    on:click={() => ($showReportProblem = true)}
+    disabled={$zoom != null && $zoom < 14}
+    title={$zoom != null && $zoom < 14
+      ? "Please zoom in further to show the problem"
+      : undefined}
+  >
     <i class="fa-solid fa-triangle-exclamation"></i>
     Report problem
   </button>
