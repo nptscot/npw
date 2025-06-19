3b7a167f3534c6302dea8a831e681aae4d9ff53d
robinlovelace <rob00x@gmail.com>
2025-06-19 18:08:17 +0100

    Update beta wording

--- web/src/common/Beta.svelte
+++ web/src/common/Beta.svelte
@@ -23,21 +23,10 @@
 
 <Modal bind:show>
   <h1>Network Planning Workspace</h1>
-  <p>
-    This tool utilises various datasets, some of which have <a
-      href="manual.html#known-limitations"
-      target="_blank"
-    >
-      limitations
-    </a>
-    .
-  </p>
-  <p>
-    Demand data: Commute layer results are based on 2011 Census data, and other
-    trip purpose results are based on pre-pandemic data. These model-based
-    estimates may not reflect current patterns which have changed since the
-    pandemic. Updated datasets will be included in future versions.
-  </p>
+				<p>The Network Planning Workspace (part of the <a href="https://npt.scot" target="blank">NPT</a>) for Scotland is a web-based application designed to support strategic cycle network planning and investment decisions, primarily for use by local authorities.</p>
+				<p>This tool utilises various datasets, some of which have <a href="manual.html#known-limitations" target="blank">limitations</a>. Users should always verify outputs against the most up to date geospatial data held by the local authority before proceeding with detailed design or investment decisions.</p>
+				<p>Please note that the commute layer results are based on 2011 Census data, and other trip purpose results are based on pre-pandemic data. These model-based estimates may not reflect current patterns which have changed since the pandemic.</p>
+				<p>Users are solely responsible for how they interpret and use the data. If you identify any errors or have concerns about the data or tool, please use the feedback form to report or provide feedback.</p>
   <p>
     Use the "Report problem" button in the bottom-left corner of the map if you
     find problems, or email the maintainer at <a
